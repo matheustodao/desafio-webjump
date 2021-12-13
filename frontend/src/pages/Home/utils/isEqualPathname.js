ed68266b744c6d122f986e402d27b18ce921bf23
Matheus Todao Bitbucket <todaomatheus@hotmail.com>
2021-12-12 21:47:25 -0300

    backend

--- frontend/src/pages/Home/utils/isEqualPathname.js
+++
@@ -1,6 +0,0 @@
-export default function historyNavigation(array, name) {
-  const index = array.findIndex((item) => (item === name && item));
-  const path = array.slice(0, index + 1);
-
-  return `/${path.join().replaceAll(',', '/')}`;
-}
--- frontend/src/pages/Home/utils/removeSpecialCharacters.js
+++
@@ -1,3 +0,0 @@
-export default function removeSpecialCharacters(value) {
-  return value.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
-}
