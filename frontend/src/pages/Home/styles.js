0e4190bb7d8f342b62b288af87edff42e471d82b
Matheus Todao Bitbucket <todaomatheus@hotmail.com>
2021-12-13 18:00:30 -0300

    docs: remove style of product not found on home/styles

--- frontend/src/pages/Home/styles.js
+++ frontend/src/pages/Home/styles.js
@@ -212,56 +212,6 @@ export const ListProducts = styled.div`
     display: none;
   }
 
-  .product__not__found {
-    position: absolute;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-    text-align: center;
-    h4 {
-      width: 29.9rem;
-      color: #3C3B3B;
-      font-size: 1.8rem;
-    }
-
-    div {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      p {
-        color: #4E4E4E;
-        margin-top: 1rem;
-        margin-bottom: 3rem;
-        font-size: 1.4rem;
-      }
-
-      button {
-        padding: 1.2rem 2.4rem;
-        background: #04D1D2;
-        font-weight: 700;
-        color: #FFF;
-        border-radius: 4px;
-        border: none;
-        box-shadow: 0px 2px 7px rgba(65, 185, 185, 0.53);
-        font-size: 1.4rem;
-        cursor: pointer;
-        width: 20rem;
-
-        &.last__button {
-          background: #dddd;
-          margin-top: .8rem;
-          color: #818181;
-          box-shadow: none;
-          font-weight: 400;
-          width: 18rem;
-
-        }
-      }
-    }
-  }
-
   /* @Media queries */
 
   @media screen and (max-width: 1247px) {
